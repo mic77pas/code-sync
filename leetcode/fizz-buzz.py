0ae6f2307a22735f3b3923a3d2adad48b9818e06
Michael Pasyechnyk <mic77p@gmail.com>
2025-09-29 23:38:35 -0400

    Add solution for fizz-buzz

---
+++ leetcode/fizz-buzz.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        answer = []
+        for i in range(1, n + 1):
+            answer.append(
+                "FizzBuzz" if i % 15 == 0 else
+                "Fizz" if i % 3 == 0 else
+                "Buzz" if i % 5 == 0 else
+                str(i)
+            )
+        return answer
